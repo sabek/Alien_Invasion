fc3996c9d3caa09ee35279a99b34153dbfadfb8f
sabek <ben.croswell@gmail.com>
2017-04-20 16:02:25 -0400

    Create scoreboard.py

---
+++ scoreboard.py
@@ -0,0 +1,56 @@
+import pygame.font
+
+class Scoreboard():
+    """A Class for scoring information"""
+    
+    def __init__(self, ai_settings, screen, stats):
+        """Initialize score attributes"""
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.ai_settings = ai_settings
+        self.stats = stats
+        
+        #Font settings for SCORE
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+        
+        #Prepare initial score Image
+        self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
+        
+    def prep_score(self):
+        """Turn the score into a rendered image"""
+        rounded_score = int(round(self.stats.score, -1))
+        score_str = "{:,}".format(rounded_score)
+        self.score_image = self.font.render(score_str, True, self.text_color, self.ai_settings.bg_color)
+        
+        #Display at top RIGHT
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+        
+    def prep_high_score(self):
+        """Turn high score into a rendered image"""
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.ai_settings.bg_color)
+        
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+        
+    def show_score(self):
+        """Draw score to the screen"""
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+        
+    def prep_level(self):
+        """Turn level into rendered image"""
+        self.level_image = self.font.render(str(self.stats.level), True, self.text_color, self.ai_settings.bg_color)
+        
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+        
