6a755821c7038ddac481b03b8ad0c173e60b81ee
sabek <ben.croswell@gmail.com>
2017-04-20 15:53:03 -0400

    Create alien_invasion.py

---
+++ alien_invasion.py
@@ -0,0 +1,46 @@
+import sys
+import pygame
+from pygame.sprite import Group
+from settings import Settings
+from game_stats import GameStats
+from ship import Ship
+from alien import Alien
+import game_functions as gf
+from button import Button
+from scoreboard import Scoreboard
+
+
+def run_game():
+    # Initialize game and create a screen object
+    pygame.init()
+    ai_settings = Settings()
+    
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    
+    # Draw start button
+    play_button = Button(ai_settings, screen, "Play")
+    
+    # Make a ship, bullets, aliens and stats
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+    
+    # Create a fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    
+    # Start the main loop for the game.
+    while True:
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, 
+                              ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+        
+run_game()
